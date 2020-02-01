198e5b0a7b0c229fe62e920b053c82698dca321c
Joe Haefling <josephhaefling@gmail.com>
2020-02-01 12:37:35 -0700

    Add wallet and dashboard functionality

--- main.js
+++ main.js
@@ -4,24 +4,28 @@ var closeTab = document.querySelector('#close-tab');
 closeTab.addEventListener('click', closeWelcomeMessage);
 
 function closeWelcomeMessage() {
-  welcome.classList.add('close-welcome');
+  welcome.classList.toggle('close-window');
 }
 
+// Wallet Button
 var walletBtn = document.querySelector('#wallet-button');
+var walletViewCol = document.querySelector('main');
+
+function walletView() {
+  console.log('hi');
+  walletBtn.classList.add('button-selected');
+  walletViewCol.classList.add('close-window');
+  dashboardBtn.classList.remove('button-selected');
+}
 
 walletBtn.addEventListener('click', walletView);
 
-
-function walletView () {
-  walletBtn.classList.toggle('button-selected');
-}
-
-
-var dashboardBtn = document.querySelector('#dashboard-button ');
-
-dashboardBtn.addEventListenter('click', selectDashboard);
-
+// Dashboard Button
+var dashboardBtn = document.querySelector('#dashboard-button');
 
 function selectDashboard() {
-
+  dashboardBtn.classList.add('button-selected');
+  walletBtn.classList.remove('button-selected');
+  walletViewCol.classList.remove('close-window');
 }
+dashboardBtn.addEventListener('click', selectDashboard);
