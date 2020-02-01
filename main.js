12b3fb40098084c9cafa5492e4caf471f89d71c8
Joe Haefling <josephhaefling@gmail.com>
2020-02-01 10:50:13 -0700

    Add blue boarder to wallet on click

--- main.js
+++ main.js
@@ -6,3 +6,22 @@ closeTab.addEventListener('click', closeWelcomeMessage);
 function closeWelcomeMessage() {
   welcome.classList.add('close-welcome');
 }
+
+var walletBtn = document.querySelector('#wallet-button');
+
+walletBtn.addEventListener('click', walletView);
+
+
+function walletView () {
+  walletBtn.classList.toggle('button-selected');
+}
+
+
+var dashboardBtn = document.querySelector('#dashboard-button ');
+
+dashboardBtn.addEventListenter('click', selectDashboard);
+
+
+function selectDashboard() {
+
+}
