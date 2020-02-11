7fcc7a56bc4c0208800045e1a435af343a48c1c5
Joe Haefling <josephhaefling@gmail.com>
2020-02-11 16:07:39 -0700

    Refactor code

--- main.js
+++ main.js
@@ -1,35 +1,29 @@
 var welcome = document.querySelector('#welcome-message');
 var closeTab = document.querySelector('#close-tab');
+var dashboardBtn = document.querySelector('#dashboard-button');
+var walletBtn = document.querySelector('#wallet-button');
+var walletViewCol = document.querySelector('main');
+var mainWallet = document.querySelector('#main-wallet');
+var navBarBtn = document.querySelector('nav');
 
+navBarBtn.addEventListener('click', changeView);
 closeTab.addEventListener('click', closeWelcomeMessage);
+
 
 function closeWelcomeMessage() {
   welcome.classList.toggle('close-window');
 }
 
-// Wallet Button
-var walletBtn = document.querySelector('#wallet-button');
-var walletViewCol = document.querySelector('main');
-var mainWallet = document.querySelector('#main-wallet');
-
-function walletView() {
-  console.log('hi');
-  walletBtn.classList.add('button-selected');
-  walletViewCol.classList.add('close-window');
-  dashboardBtn.classList.remove('button-selected');
-  mainWallet.classList.remove('close-window');
+function changeView(event) {
+  if (event.target.id === 'wallet-button') {
+    walletBtn.classList.add('button-selected');
+      walletViewCol.classList.add('close-window');
+      dashboardBtn.classList.remove('button-selected');
+      mainWallet.classList.remove('close-window');
+  } else if (event.target.id === 'dashboard-button'){
+    dashboardBtn.classList.add('button-selected');
+      walletBtn.classList.remove('button-selected');
+      walletViewCol.classList.remove('close-window');
+      mainWallet.classList.add('close-window');
+  }
 }
-
-walletBtn.addEventListener('click', walletView);
-
-// Dashboard Button
-var dashboardBtn = document.querySelector('#dashboard-button');
-
-function selectDashboard() {
-  dashboardBtn.classList.add('button-selected');
-  walletBtn.classList.remove('button-selected');
-  walletViewCol.classList.remove('close-window');
-  mainWallet.classList.add('close-window');
-
-}
-dashboardBtn.addEventListener('click', selectDashboard);
